b445884e9c58013440a1800ac818947e1b288f92
gustavomeza <gustaavomeza@gmail.com>
2019-07-12 16:14:14 -0500

    carga de plantillas dinamico

--- controladores/plantilla.controlador.php
+++ controladores/plantilla.controlador.php
@@ -6,7 +6,34 @@ class ControladorPlantilla{
 
 		include "vistas/plantilla.php";
 
-	}	
+	}
+	
+	public function loadView(){
+
+	
+		if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok"){
+	  
+		 echo '<div class="wrapper">';
+		  include "vistas/modulos/cabezote.php";
+		  include "vistas/modulos/menu.php";
+		  if(isset($_GET["ruta"])){
+			if($_GET["ruta"] == IS_DIR){
+				include "vistas/modulos/".IS_DIR.".php";
+			}else{
+			  include "vistas/modulos/404.php";
+			}
+		  }else{
+			include "vistas/modulos/inicio.php";
+		  }
+		  include "vistas/modulos/footer.php";
+		  echo '</div>';
+		}else{
+		  include "vistas/modulos/login.php";
+		}
+		
+
+
+	}
 
 
 }
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -29,64 +29,8 @@ session_start();
   <script src="vistas/bower_components/datatables.net-bs/js/dataTables.responsive.min.js"></script>
   <script src="vistas/bower_components/datatables.net-bs/js/responsive.bootstrap.min.js"></script>
 </head>
-
-
 <body class="hold-transition skin-blue sidebar-collapse sidebar-mini login-page">
-
-
-  <?php
-  
-  if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok"){
-
-   echo '<div class="wrapper">';
- 
-
-    include "modulos/cabezote.php";
-
-   
-    include "modulos/menu.php";
-
-
-    if(isset($_GET["ruta"])){
-
-      if($_GET["ruta"] == "inicio" ||
-         $_GET["ruta"] == "usuarios" ||
-         $_GET["ruta"] == "categorias" ||
-         $_GET["ruta"] == "productos" ||
-         $_GET["ruta"] == "clientes" ||
-         $_GET["ruta"] == "ventas" ||
-         $_GET["ruta"] == "crear-venta" ||
-         $_GET["ruta"] == "reportes" ||
-         $_GET["ruta"] == "salir"){
-
-        include "modulos/".$_GET["ruta"].".php";
-
-      }else{
-
-        include "modulos/404.php";
-
-      }
-
-    }else{
-
-      include "modulos/inicio.php";
-
-    }
-
-  
-    include "modulos/footer.php";
-
-    echo '</div>';
-
-  }else{
-
-    include "modulos/login.php";
-
-  }
-
-  ?>
-
-
+<?php   $c  = ControladorPlantilla::loadView(); ?>
 <script src="vistas/js/plantilla.js"></script>
 </body>
 </html>
