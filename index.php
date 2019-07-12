ad9f662f6daf4eb6ebf2f8ae91f656e5dfcfee2a
gustavomeza <gustaavomeza@gmail.com>
2019-07-12 14:57:41 -0500

    Correccion de sobre carga de modulos y controladores

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 
 
-
-
-
-
 <?php
-
-
-
 
 $dir_ini = "/pos/";
 $host = $_SERVER["HTTP_HOST"].$dir_ini;
@@ -20,33 +13,15 @@ define('IS_DIR', $dir);
 require_once "controladores/plantilla.controlador.php";
 require_once "modelos/plantilla.modelo.php";
 
+if(IS_DIR != '')
+    $DIR = IS_DIR;
+else
+    $DIR = "inicio";
 
 
-if(IS_DIR != ''){
-    echo 1;
-}
+require_once "controladores/".$DIR.".controlador.php";
+require_once "modelos/".$DIR.".modelo.php";
 
-
-
-require_once "controladores/categorias.controlador.php";
-require_once "controladores/productos.controlador.php";
-require_once "controladores/usuarios.controlador.php";
-require_once "controladores/clientes.controlador.php";
-require_once "controladores/ventas.controlador.php";
-
-
-
-
-
-require_once "modelos/categorias.modelo.php";
-require_once "modelos/productos.modelo.php";
-require_once "modelos/usuarios.modelo.php";
-require_once "modelos/clientes.modelo.php";
-
-
-/*
-TERMIN LA MALA METODOLOGIA DE ESTRUCTURA, CREARA SOBRE CARGA EN UN FUTURO
-*/
 
 $plantilla = new ControladorPlantilla();
 
